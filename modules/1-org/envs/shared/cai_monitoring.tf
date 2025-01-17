2754aba18b327ca00687f396b4142308b46901af
Roy Liu <carsomyr@gmail.com>
2025-01-16 23:58:45 -0500

    Import the 1-org stage/module from the terraform-example-foundation repository

---
+++ modules/1-org/envs/shared/cai_monitoring.tf
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+module "cai_monitoring" {
+  source                = "../../modules/cai-monitoring"
+  count                 = var.enable_scc_resources_in_terraform ? 1 : 0
+  org_id                = local.org_id
+  billing_account       = local.billing_account
+  project_id            = module.scc_notifications.project_id
+  location              = local.default_region
+  build_service_account = "projects/${module.scc_notifications.project_id}/serviceAccounts/${google_service_account.cai_monitoring_builder[0].email}"
+}
