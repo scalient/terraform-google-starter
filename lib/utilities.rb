0888fb31ab92972c1773e0fbe1a1e7038b9f5bde
Roy Liu <carsomyr@gmail.com>
2025-01-19 08:07:49 -0500

    Improve the `ensure_permissions` Rake task

--- lib/utilities.rb
+++ lib/utilities.rb
@@ -129,7 +129,26 @@ module Utilities
 
     organization_id = JSON.parse(child_output)["organization_id"]["value"]
 
-    roles.each do |role|
+    heredoc_content = <<EOS
+.bindings[] | select(.members[] | index("user:#{authenticated_user}")) | .role
+EOS
+
+    Open3.pipeline_rw(
+      ["gcloud", "organizations", "get-iam-policy", organization_id.to_s, "--format", "json"],
+      ["jq", heredoc_content],
+    ) do |first_stdin, last_stdout, wait_thrs|
+      first_stdin.close
+      child_output = last_stdout.read
+
+      wait_thrs.each do |wait_thr|
+        if (status = wait_thr.value) != 0
+          raise SystemCallError.new("Terraform call failed", status.exitstatus)
+        end
+      end
+    end
+
+    # Set subtract to determine which roles the authenticated user is missing for Terraform to finish without 403's.
+    (roles - child_output.chomp.split("\n", -1).map { |line| JSON.parse(line) }).each do |role|
       rake_instance.send(
         :sh, "gcloud", "organizations", "add-iam-policy-binding", organization_id,
         "--member", "user:#{authenticated_user}",
